3b040b9ea4b0560c23e19a70e9b8b17e490fc3db
Ulf Hansson <ulf.hansson@linaro.org>
2014-10-16 16:18:51 +0200

    mmc: core: Remove mmc_free_ext_csd()
    Let callers of mmc_free_ext_csd() do kfree() directly instead.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -726,12 +726,6 @@ out:
 	return err;
 }
 
-static inline void mmc_free_ext_csd(u8 *ext_csd)
-{
-	kfree(ext_csd);
-}
-
-
 static int mmc_compare_ext_csds(struct mmc_card *card, unsigned bus_width)
 {
 	u8 *bw_ext_csd;
@@ -786,7 +780,7 @@ static int mmc_compare_ext_csds(struct mmc_card *card, unsigned bus_width)
 		err = -EINVAL;
 
 out:
-	mmc_free_ext_csd(bw_ext_csd);
+	kfree(bw_ext_csd);
 	return err;
 }
 
