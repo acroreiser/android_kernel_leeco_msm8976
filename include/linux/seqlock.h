3e8c3fb8835f18b3fab5c5620ae3a5409506c2ec
Peter Zijlstra <peterz@infradead.org>
2015-05-27 11:09:36 +0930

    BACKPORT: seqlock: Introduce raw_read_seqcount_latch()
    Because with latches there is a strict data dependency on the seq load
    we can avoid the rmb in favour of a read_barrier_depends.
    
    Suggested-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- include/linux/seqlock.h
+++ include/linux/seqlock.h
@@ -29,6 +29,7 @@
 #include <linux/spinlock.h>
 #include <linux/preempt.h>
 #include <linux/lockdep.h>
+#include <linux/compiler.h>
 #include <asm/processor.h>
 
 /*
@@ -248,6 +249,11 @@ static inline void write_seqcount_end(seqcount_t *s)
 	seqcount_release(&s->dep_map, 1, _RET_IP_);
 	smp_wmb();
 	s->sequence++;
+}
+
+static inline int raw_read_seqcount_latch(seqcount_t *s)
+{
+	return lockless_dereference(s->sequence);
 }
 
 /**
